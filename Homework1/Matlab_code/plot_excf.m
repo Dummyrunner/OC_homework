ab94630b54c07830caef76f1958d9dafdd167694
Daniel Bergmann <bergmann.da@gmx.de>
2020-12-11 20:04:41 +0100

    computations and plots + saved pdf for f),g)

--- Homework1/Matlab_code/plot_excf.m
+++ Homework1/Matlab_code/plot_excf.m
@@ -5,6 +5,9 @@ num =  struct;
 [ana.x1,ana.x2,ana.u] = extract_xu(yopt_ana);
 [num.x1,num.x2,num.u] = extract_xu(yopt_num);
 
+% % difference between analytic and numeric solution
+% e.x1 = ana.x1 - num.x1;
+
 % Plot all trajectories of num. and ana. sol.
 
 % Plot window measures: % does not work currently
@@ -12,7 +15,7 @@ num =  struct;
 % width = 2000;
 % height = 1500;
 
-fig_f1 = figure(1); clf;
+fig_f1 = figure(10); clf;
 sgtitle('State and input trajectories of optimal solution')
 subplot(2,2,1)
 hold on
@@ -45,7 +48,33 @@ hold off
 legend;
 
 % Plot Comparison. analytic vs numerical sol.
-fig_f2 = figure(2); clf;
+fig_f2 = figure(11); clf;
+sgtitle('Comparison analytic vs. numerical solution')
+subplot(3,1,1)
+hold on
+title('State x1 over time, analytical vs. numerical solution')
+plot(ana.x1,'DisplayName','x1_{ana}')
+plot(num.x1,'DisplayName','x1_{num}')
+hold off
+legend;
+
+subplot(3,1,2)
+hold on
+title('State x1 over time, analytical vs. numerical solution')
+plot(ana.x2,'DisplayName','x2_{ana}')
+plot(num.x2,'DisplayName','x2_{num}')
+hold off
+legend;
+
+subplot(3,1,3)
+hold on
+title('Input u over time, analytical vs. numerical solution')
+plot(ana.u,'DisplayName','u_{ana}')
+plot(num.u,'DisplayName','u_{num}')
+hold off
+legend;
+
+fig_f3 = figure(12); clf;
 sgtitle('Comparison analytic vs. numerical solution')
 subplot(3,1,1)
 hold on
@@ -74,5 +103,5 @@ legend;
 %% Save plots to file
 saveas(fig_f1, '..\plots\exc_f1.pdf')
 saveas(fig_f2, '..\plots\exc_f2.pdf')
-
+disp('Generated plots saved to file')
 
--- Homework1/Matlab_code/run_excercises.m
+++ Homework1/Matlab_code/run_excercises.m
@@ -1,5 +1,5 @@
-%% exc f)
-% init given data
+
+%% init data + exact discretization of system dynamics
 Acont = [0 1; 2 1];
 Bcont = ones(2,1);
 Q= eye(2);
@@ -16,6 +16,8 @@ sys_c = ss(Acont,Bcont,eye(2),0);
 sys_d = c2d(sys_c,h);
 [Ad,Bd,~,~] = ssdata(sys_d);
 
+
+%% exc f)
 [H,f,Aeq,beq] = lqr_ecfh2quadprog(Ad, Bd, Q, R, N,x0);
 
 %  Compute optimum analytically via KKT
@@ -25,5 +27,61 @@ yopt_ana = Hinv*Aeq'*inv(Aeq*Hinv*Aeq')*beq;
 % Compute optimum numerically via quadprog()
 [yopt_num, fval_num] = quadprog(H,f,zeros(size(f,1)),zeros(size(f,1),1),Aeq,beq);
 
-% Call plot routine for visualization
+% Call plot script for visualization, including save to file
 plot_excf;
+disp('Exc f) done...')
+
+%% exc g)
+
+alphas = [0.1 1 10];
+% yopt = {};
+x1 = {};
+x2 = {};
+u = {};
+
+% prepare figure
+figure(3);clf;
+
+% compute opt traj. for each alpha
+for ialpha = 1:length(alphas)
+    Q = alphas(ialpha)*eye(2);
+    [H,f,Aeq,beq] = lqr_ecfh2quadprog(Ad, Bd, Q, R, N,x0);
+    Hinv = inv(H);
+    yopt = Hinv*Aeq'*inv(Aeq*Hinv*Aeq')*beq;
+    [x1{ialpha},x2{ialpha},u{ialpha}] = extract_xu(yopt);
+end
+
+% plot results for each alpha
+for ialpha = 1:length(alphas)
+    fig_g = figure(30)
+    sgtitle('Compare traj. for different $Q = \alpha I$ with $\alpha = 0.1,1,10$','Interpreter','latex');
+    
+    subplot(3,1,1)
+    hold on
+    title('State x1 over time, analytical solution')
+    plot(x1{ialpha},'DisplayName',['x1, $\alpha=$',num2str(alphas(ialpha))]);
+    hold off
+    legend('Interpreter','latex');
+    
+    subplot(3,1,2)
+    hold on
+    title('State x2 over time analytical solution')
+    plot(x2{ialpha},'DisplayName',['x2, $\alpha=$',num2str(alphas(ialpha))]);
+    hold off
+    legend('Interpreter','latex');
+
+    subplot(3,1,3)
+    hold on
+    title('Input u over time analytical solution')
+    plot(u{ialpha},'DisplayName',['u, $\alpha=$',num2str(alphas(ialpha))]);
+    hold off
+    legend('Interpreter','latex');
+end
+disp('Exc g) done...')
+saveas(fig_g, '..\plots\exc_g.pdf');
+disp('Generated plots saved to file')
+
+disp('Excercise script finished successfully')
+
+
+
