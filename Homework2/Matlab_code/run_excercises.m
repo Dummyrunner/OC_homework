cad428b35533985be5c14f4151d377ba8983a6bb
Daniel Bergmann <bergmann.da@gmx.de>
2021-01-17 09:51:49 +0100

    improved comments/display

--- Homework2/Matlab_code/run_excercises.m
+++ Homework2/Matlab_code/run_excercises.m
@@ -1,6 +1,6 @@
-% Problem 1
+%% Problem 1
 
-% exc a)
+%% exc b)
 % Init Val. Function with arbitrarily chosen Init. cond.
 V0 = zeros(8,1);
 alpha = 0.9; % discount factor
@@ -25,10 +25,7 @@ f0 = [1 1 1;...
     2 2 2;...
     0 0 0];
 
-% amount to add to value function
-% motivated by the theor. part of this exc
-
-
+% Value Function Iteration
 V = V0;
 n = 1000;
 for iter = 1:n
@@ -44,34 +41,18 @@ msg = ['The Value function, obtained by iterating over ',num2str(n),' iterations
 disp(msg)
 disp(V)
 
-% determine optimal feedback
-% access = zeros(8);
-% l1 = [2 3];
-% l2 = [4 5 7];
-% l3 = 4:6;
-% l4 = 6:8;
-% l5 = [4 6];
-% l6 = [1 7 8];
-% l7 = 8;
-% l8 = 8;
-% for istate = 1:8
-% 	cmd = ['access(istate,','l',num2str(istate),') = 1'];
-%     eval(cmd);
-% end
-
-
-
 % Optimal feedback u = k(x), s. t. V(f(x,k(x)) minimal
-% k = zeros(8,1);
-% for x = 1:8
-% %     get input signal that minimizes V-Value of next state
-%     [~,idx] = min( V(f(x,:)));
-%     idx = idx(1); % in case of >1 minima
-% %   index shift
-%     k(x) = idx - 1;
-    
-% end
+k = zeros(8,1);
+for x = 1:8
+%     get input signal that minimizes V-Value of next state
+    [~,idx] = min( V(f(x,:)));
+    idx = idx(1); % in case of >1 minima
+%   index shift
+    k(x) = idx - 1;
+end
 
-% k
+msg = 'The Value Function yields the optimal state feedback k: X -> U';
+disp(msg)
+k
 
-
+% 
