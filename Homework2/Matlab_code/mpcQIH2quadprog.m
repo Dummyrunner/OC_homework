856b1a9eeae22cdb6d4e2bf562569c742354e425
Daniel Bergmann <bergmann.da@gmx.de>
2021-01-26 18:08:01 +0100

    adjusted mpcQIZ2quadprog to actual problem

--- Homework2/Matlab_code/mpcQIH2quadprog.m
+++ Homework2/Matlab_code/mpcQIH2quadprog.m
@@ -1,4 +1,4 @@
-function [H,T,Aineq,bineq,Aeq,beq] = mpcQIH2quadprog(Ad,Bd,Q,R,P,delta,N,xt,xnormbound,unormbound)
+function [H,T,Aineq,bineq,Aeq,beq] = mpcQIH2quadprog(Ad,Bd,Q,R,P,delta,N,xt,unormbound)
 %MPCQIH2QUADPROG Translates a LQR problem with quasi-infinite time horizon
 %into a quadratic program with quadratic an linear constraints.
 % 
@@ -16,27 +16,26 @@ Qs = repmat({Q},N,1);
 Rs = repmat({R},N,1);
 Qblk = blkdiag(Qs{:});
 Rblk = blkdiag(Rs{:});
-H = delta.*blkdiag(Qblk,zeros(n),Rblk);
+H = delta.*blkdiag(Qblk,P,Rblk);
 
 % express box constraints for pred. state and input in inequality constr.
-Aineq = [xnormbound.*eye(n*(N+1)) zeros(n*(N+1),m*N);...
-         -xnormbound.*eye(n*(N+1)) zeros(n*(N+1),m*N);...
-         zeros(m*N,n*(N+1)) unormbound.*eye(m*N);...
+Aineq = [zeros(m*N,n*(N+1)) unormbound.*eye(m*N);...
          zeros(m*N,n*(N+1)) -unormbound.*eye(m*N)];
-bineq = ones(2*(n*(N+1) + m*N),1);
+bineq = ones(2*(m*N),1);
 
 % express system dynamics and init. cond. in equality constr.
 Aeqx = kron(eye(N+1),eye(n));
 Aeqx(n+1:end,n+1:end) = -1.*Aeqx(n+1:end,n+1:end);
 Aeqx = Aeqx + kron(diag(ones(N,1),-1),Ad);
-lastrow = zeros(1,N+1); lastrow(end) = 1;
-Aeqx = [Aeqx; kron(lastrow,eye(n))];
+% lastrow = zeros(1,N+1); lastrow(end) = 1;
+% Aeqx = [Aeqx; kron(lastrow,eye(n))];
 
 
 Aequ = kron(eye(N),Bd);
-Aequ = [zeros(n,N*m); Aequ; zeros(n,N*m)];
+% Aequ = [zeros(n,N*m); Aequ; zeros(n,N*m)]; %OLD
+Aequ = [zeros(n,N*m); Aequ];
 
-beq = [xt; zeros((N+1)*n,1)];
+beq = [xt; zeros((N)*n,1)];
 
 Aeq = [Aeqx Aequ];
 
